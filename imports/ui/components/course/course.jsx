2d22d360471bdeef3cef4ebc3af223a34c123b12
viyanateaa <viyanateaa@gmail.com>
2017-03-02 11:36:31 +0100

    add course design elemnts

--- imports/ui/components/course/course.jsx
+++ imports/ui/components/course/course.jsx
@@ -4,7 +4,31 @@ import React, { Component, PropTypes } from 'react';
 export default class Course extends Component {
   render() {
     return (
-      <li>{this.props.course.title}</li>
+      
+
+
+      <div class ="content">
+		<section id= "course_1">
+				<h1>3D-modellering och siktanalys</h1>
+				
+				<nav id= "snabbfakta">
+					<ul class="snabbfakta_course_1">
+						<li>7,5hp</li>
+						<li>Avancerad nivå</li>
+						<li>Vårtermin 2018</li>
+					</ul>
+				</nav>
+				
+
+				<h2>OM KURSEN</h2>
+				<p>Kursen behandlar 3D-visualisering av terräng baserat på olika slags höjddata, men även representation av byggda miljöer i 3D; i det senare fallet med CAD som ett viktigt inslag. För området relevanta analysmetoder, såsom olika former av siktanalys för att utvärdera effekter av befintlig och planerad bebyggelse, ingår också i kursens innehåll.</p>
+		</section>
+
+		<section id= "course_2"></section>
+		<section id= "course_3"></section>
+		<section id= "course_4"></section>
+		<section id= "course_5"></section>
+	</div>
     );
   }
 }

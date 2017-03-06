c03245e28e251989aed88dfb52c2b9a8ee42eff8
syltman <mcoquand@gmail.com>
2017-03-06 10:00:55 +0100

    still stuck, no magic happening

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import { Router, Route, browserHistory } from 'react-router';
 
 // route components
 import App from '../../ui/App.jsx';
 import CreateCourse from '../../ui/routes/createCourse/CreateCourse.jsx';
-import NotFoundPage from '../../ui/routes/not-found/not-found.jsx';
+import NotFoundPage from '../../ui/routes/notFound/notFound.jsx';
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <Route path="/create" component={CreateCourse}/>
+      <Route path="create" component={CreateCourse}/>
       <Route path="*" component={NotFoundPage}/> //NotFoundPage
     </Route>
   </Router>

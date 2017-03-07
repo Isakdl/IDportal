b40cfce3920e4e3018fb8f396991b27a9b4794e0
syltman <mcoquand@gmail.com>
2017-03-07 16:14:54 +0100

    still broken..

--- imports/ui/routes/createCourse/CreateCourse.jsx
+++ imports/ui/routes/createCourse/CreateCourse.jsx
@@ -6,25 +6,27 @@ import constants from './../../../constants/apiConstants';
 // Task component - represents a single todo item
 export default class CreateCourse extends Component {
 
-  handleSubmit()
+  handleSubmit(event)
   {
-    const title = ReactDOM.findDOMNode(this.refs.name).value.trim();
-    const hp = ReactDOM.findDOMNode(this.refs.hp).value.trim();
-    const speed = ReactDOM.findDOMNode(this.refs.speed).value.trim();
-    const description = 
-      ReactDOM.findDOMNode(this.refs.description).value.trim();
-    const url = ReactDOM.findDOMNode(this.refs.url).value.trim();
-    const period = ReactDOM.findDOMNode(this.refs.period).value.trim();
-    Meteor.call(constants.COURSES_INSERT, title, hp, speed, description, url,
+    const target = event.target;
+    const title = target.title.trim();
+    const ects = target.ects.trim();
+    const speed = target.speed.trim();
+    const url = target.url.trim();
+    const period = target.period.trim();
+    
+    if (failed)
+    {
+      this.setState(
+      {
+        
+      });
+    }
+    Meteor.call(constants.COURSES_INSERT, title, ects, speed, description, url,
         period);
   
   }
-  renderOptions() 
-  {
-    for (i=1; i<4; i++) 
-    {
-    }
-  }
+
   render() {
     return (
         <div className="containerCreateCourse">
@@ -32,24 +34,24 @@ export default class CreateCourse extends Component {
 
           <form className="new-course" >
             Course name:
-            <input className="courseInput" type="text" ref="name" 
+            <input className="courseInput" type="text" name="title" 
               placeholder="Prototyputveckling av mobila applikationer" min="4" 
               max="250"/>
             ECTS:
-            <input className="courseInput" type="text" ref="ects" min="1" max="3"
+            <input className="courseInput" type="text" name="ects" min="1" max="3"
               placeholder="7.5"/>
             Speed:
-            <input className="courseInput" type="text" ref="speed" min="3" max="3"
+            <input className="courseInput" type="text" name="speed" min="3" max="3"
               placeholder="50%"/>
             Description:
-            <input className="courseInput" type="text" ref="description" max="2000"
+            <input className="courseInput" type="text" name="description" max="2000"
               placeholder="Example"/>
             Webpage:
-            <input className="courseInput" type="text" ref="url" max="2000"
+            <input className="courseInput" type="text" name="url" max="2000"
               placeholder="https://www.umu.se/utbildning/kurser/prototyputveckling-for-mobila-applikationer/"/>
             Period:
 
-            <select className="courseInput">
+            <select className="courseInput" name="period">
               <option value="block1">Block 1</option>
               <option value="block1a">Block 1a</option>
               <option value="block1b">Block 1b</option>
@@ -64,7 +66,7 @@ export default class CreateCourse extends Component {
               <option value="block4b">Block 4b</option>
             </select>
   
-            <input type="button" onClick={this.handleSubmit.bind(this)} 
+            <input type="button" onClick={() => this.handleSubmit()} 
               value="Submit"/>
           </form>
         </div>
